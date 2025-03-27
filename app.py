f385aaa897f6545c97ce371fc1b38dcb17c79eb8
Jason MacLeod <jason.d.macleod@gmail.com>
2025-03-27 14:57:23 -0700

    fix: add aws account via cdk environment variable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 import aws_cdk as cdk
 
@@ -7,11 +8,17 @@ from aws_cdk_multiple_stacks_python.multistack_stack import MultistackStack
 app = cdk.App()
 
 MultistackStack(
-    app, "MyWestCdkStack", env=cdk.Environment(region="us-west-1"), encrypt_bucket=False
+    app,
+    "MyWestCdkStack",
+    env=cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="us-west-1"),
+    encrypt_bucket=False,
 )
 
 MultistackStack(
-    app, "MyEastCdkStack", env=cdk.Environment(region="us-east-1"), encrypt_bucket=True
+    app,
+    "MyEastCdkStack",
+    env=cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="us-east-1"),
+    encrypt_bucket=True,
 )
 
 app.synth()
